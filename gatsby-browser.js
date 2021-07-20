09ee1ad4814546162df773776452a212c7a1003d
situ2001 <yongcong2001@outlook.com>
2021-07-20 18:38:03 +0800

    MyImg: fix potential loss of style

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,26 +1,2 @@
-import React from 'react';
-import { MDXProvider } from '@mdx-js/react';
-import * as styles from './src/styles/mdx.module.css';
-
 // import prism theme
 require('prism-themes/themes/prism-vs.css');
-
-const MyImg = (props) => (
-  <img 
-    className={styles.myImg}
-    alt=""
-    {...props}
-  />
-);
-
-const components = {
-  img: MyImg,
-};
-
-export const wrapRootElement = ({ element }) => {
-  return (
-    <MDXProvider components={components}>
-      {element}
-    </MDXProvider>
-  )
-};
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { MDXProvider } from "@mdx-js/react"
 import Layout from '../components/Layout';
 import Container from '../components/Container';
+import * as styles from '../styles/mdx.module.css';
+
+const MyImg = (props) => (
+  <img 
+    className={styles.myImg}
+    alt=""
+    {...props}
+  />
+);
+
+const components = {
+  img: MyImg,
+};
 
 export default function Component({ pageContext, location }) {
   return (
@@ -11,9 +25,9 @@ export default function Component({ pageContext, location }) {
           <h1>{pageContext.title}</h1>
           <p>Posted: {pageContext.date}</p>
           <hr/>
-          <MDXRenderer>
-            {pageContext.body}
-          </MDXRenderer>
+          <MDXProvider components={components}>
+            <MDXRenderer>{pageContext.body}</MDXRenderer>
+          </MDXProvider>
         </div>
       </Container>
     </Layout>
