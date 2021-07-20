8c388f02b6192d60882106eee1df3a234bad91c9
situ2001 <yongcong2001@outlook.com>
2021-07-20 11:17:36 +0800

    blog: update template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from '../components/Layout';
 
-export default function Component({ pageContext }) {
+export default function Component({ pageContext, location }) {
   return (
-    <main>
-      <h1>{pageContext.slug}</h1>
-      <MDXRenderer>
-        {pageContext.body}
-      </MDXRenderer>
-    </main>
+    <Layout location={location}>
+      <div>
+        <h1>{pageContext.title}</h1>
+        <p>Posted: {pageContext.date}</p>
+        <hr/>
+        <MDXRenderer>
+          {pageContext.body}
+        </MDXRenderer>
+      </div>
+    </Layout>
   );
 };
