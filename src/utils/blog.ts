7ab8bab82e5cfdaa1af595dae06819a9891e7ba0
situ2001 <yongcong2001@outlook.com>
2021-07-27 17:46:28 +0800

    refactor(src/template): fix styles according to eslint

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -12,17 +12,17 @@ export type HeadingResult = {
 
 // a function that perform HeadingRaw => HeadingResult
 export const getFormattedHeadings = (headings: HeadingRaw[]): HeadingResult[] => {
-  const o = Array<HeadingResult>();
-  for (const heading of headings) {
-    if (heading.depth === 1) {
-      continue;
-    }
-
-    heading['children'] = [];
-    if (heading.depth === 2) {
-      o.push(heading as HeadingResult);
-    } else {
-      o[o.length - 1].children.push(heading as HeadingResult);
+  const o = [];
+  for (let i = 0; i < headings.length; i += 1) {
+    const heading = headings[i];
+    if (heading.depth !== 1) {
+      // eslint-disable-next-line dot-notation
+      heading['children'] = [];
+      if (heading.depth === 2) {
+        o.push(heading as HeadingResult);
+      } else {
+        o[o.length - 1].children.push(heading as HeadingResult);
+      }
     }
   }
 
