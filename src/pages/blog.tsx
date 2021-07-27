c9834b02c324d6f3490504230334e973b926db83
situ2001 <yongcong2001@outlook.com>
2021-07-27 13:35:10 +0800

    refactor(pages/blog): remove unused import

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
 import BlogCard from '../components/BlogCard';
