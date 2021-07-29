d16e23b5317d54f42c8c3cae9af99222b33e2897
situ2001 <yongcong2001@outlook.com>
2021-07-29 10:08:09 +0800

    feat: support pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,7 +25,7 @@ exports.createPages = async ({ actions, graphql }) => {
 
   // read all blog posts
   data.allMdx.nodes.forEach((node) => {
-    const pathName = node.frontmatter.permalink ?? `/${node.parent.name}` ?? null;
+    const pathName = `/${node.frontmatter.permalink ?? node.parent.name ?? null}`;
     const { id } = node;
     const { title, date } = node.frontmatter;
 
@@ -40,5 +40,22 @@ exports.createPages = async ({ actions, graphql }) => {
         },
       });
     }
+  });
+
+  // create paginated list for blog posts
+  const maxPostsPerPage = 10;
+  const postCount = data.allMdx.nodes.length;
+  const pageCount = Math.ceil(postCount / maxPostsPerPage);
+  Array(pageCount).fill(undefined).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? '/blog' : `/blog/page/${i + 1}`,
+      component: postList,
+      context: {
+        totalPage: pageCount,
+        currentPage: i + 1,
+        limit: maxPostsPerPage,
+        skip: maxPostsPerPage * i,
+      },
+    });
   });
 };
