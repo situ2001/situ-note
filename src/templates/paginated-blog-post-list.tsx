553ad4ec358441483d7950e0209638e5ec15425a
situ2001 <yongcong2001@outlook.com>
2021-07-29 10:06:00 +0800

    template: support pagination

--- src/templates/paginated-blog-post-list.tsx
+++ src/templates/paginated-blog-post-list.tsx
@@ -3,36 +3,42 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Container from '../components/Container';
 import BlogCard from '../components/BlogCard';
+import Pagination from '../components/Pagination';
 
 type Props = {
-  location: Location,
   data: {
     allMdx: {
       nodes: {
-        parent: {
-          name: string,
-        },
         id: string,
         frontmatter: {
           date: string,
-          permalink: string,
           title: string,
+          permalink: string,
+        },
+        parent: {
+          name: string,
         },
       }[],
-    },
+    }
+  },
+  pageContext: {
+    totalPage: number,
+    currentPage: number,
+    limit: number,
+    skip: number,
   },
 };
 
-const Blog = (props: Props) => {
-  const { data, location } = props;
-
+export default function Component({ data, pageContext }: Props) {
+  const { totalPage, currentPage } = pageContext;
   return (
+    // eslint-disable-next-line no-restricted-globals
     <Layout location={location}>
       <Container>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           {
             data.allMdx.nodes.map((node) => {
-              let link: string = node.frontmatter.permalink ?? `/${node.parent.name}` ?? null;
+              let link: string = `/${node.frontmatter.permalink}` ?? `/${node.parent.name}` ?? null;
               link = `/blog${link}`;
               const { title } = node.frontmatter;
               const { date } = node.frontmatter;
@@ -41,28 +47,27 @@ const Blog = (props: Props) => {
               );
             })
           }
+          <Pagination totalPage={totalPage} currentPage={currentPage} />
         </div>
       </Container>
     </Layout>
   );
-};
-
-export default Blog;
+}
 
 export const query = graphql`
-  {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+  query PaginatedBlogPostList($limit: Int, $skip: Int) {
+    allMdx(skip: $skip, limit: $limit, sort: {order: DESC, fields: frontmatter___date}) {
       nodes {
+        id
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          permalink
+        }
         parent {
           ... on File {
             name
           }
-        }
-        id
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          permalink
-          title
         }
       }
     }
