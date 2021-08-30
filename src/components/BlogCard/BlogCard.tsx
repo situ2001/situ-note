11e40a56a94915d72c719bbd6a6d5a12156bd4a4
situ2001 <yongcong2001@outlook.com>
2021-08-30 17:12:07 +0800

    Revert "style(BlogCard): remove button"
    This reverts commit 38f71b0ed66dfafbbc3ee8d86706b7c8864c02ac.

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -9,17 +9,35 @@ type Props = {
   description: string | null;
 };
 
+const ReadBtn = styled(Link)`
+  & {
+    color: black;
+    background-color: white;
+    text-decoration: none;
+    border: 2px solid black;
+    border-radius: 2px;
+    padding: 0 20px;
+    transition: background-color 0.2s ease-in-out;
+  }
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
+
 const BlogCard = ({ title, date, link, description }: Props) => (
   <div className="shadow hover:shadow-lg transform hover:scale-105 transition-transform delay-50 p-4 rounded w-11/12 flex flex-col md:flex-row flex-wrap items-center mb-4">
     <div className="flex-grow break-words">
-      <Link
-        className="text-3xl block max-w-max text-center md:text-left mb-1"
-        to={link}
-      >
+      <Link className="text-3xl block text-center md:text-left mb-1" to={link}>
         {title}
       </Link>
       <p className="text-center md:text-left mb-4">{date}</p>
       <p>{description || 'Author is so lazy that he left nothing here.'}</p>
+    </div>
+    <div className="flex-grow-0">
+      <ReadBtn className="hidden md:block rounded-full" to={link}>
+        {'>> Read'}
+      </ReadBtn>
     </div>
   </div>
 );
