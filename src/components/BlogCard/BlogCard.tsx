670f459742b86cf3e0e952d2e353675179e54d0c
situ2001 <yongcong2001@outlook.com>
2021-08-30 17:12:50 +0800

    style(BlogCard): remove button

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'gatsby';
-import styled from 'styled-components';
 import * as React from 'react';
 
 type Props = {
@@ -9,22 +8,6 @@ type Props = {
   description: string | null;
 };
 
-const ReadBtn = styled(Link)`
-  & {
-    color: black;
-    background-color: white;
-    text-decoration: none;
-    border: 2px solid black;
-    border-radius: 2px;
-    padding: 0 20px;
-    transition: background-color 0.2s ease-in-out;
-  }
-  &:hover {
-    background-color: black;
-    color: white;
-  }
-`;
-
 const BlogCard = ({ title, date, link, description }: Props) => (
   <div className="shadow hover:shadow-lg transform hover:scale-105 transition-transform delay-50 p-4 rounded w-11/12 flex flex-col md:flex-row flex-wrap items-center mb-4">
     <div className="flex-grow break-words">
@@ -33,11 +16,6 @@ const BlogCard = ({ title, date, link, description }: Props) => (
       </Link>
       <p className="text-center md:text-left mb-4">{date}</p>
       <p>{description || 'Author is so lazy that he left nothing here.'}</p>
-    </div>
-    <div className="flex-grow-0">
-      <ReadBtn className="hidden md:block rounded-full" to={link}>
-        {'>> Read'}
-      </ReadBtn>
     </div>
   </div>
 );
