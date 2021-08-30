407868d519aeb73e19582c51c44e361e4aec5910
situ2001 <yongcong2001@outlook.com>
2021-08-30 21:32:25 +0800

    style(BlogSideBar): refine margin & padding

--- src/components/BlogSidebar.tsx
+++ src/components/BlogSidebar.tsx
@@ -5,11 +5,15 @@ const BlogSidebar = ({ currentPage, totalPage }: any) => {
   return (
     <div className="hidden md:flex flex-col justify-center md:w-3/12 shadow-lg rounded max-h-screen fixed top-0 right-0 h-screen">
       <div className="text-4xl">
-        <p className="cursor-pointer my-4 border-l-4 border-blue-300 pl-7 bg-gradient-to-l from-purple-50 to-blue-50">
+        <p className="cursor-pointer my-2 py-4 border-l-4 border-blue-300 pl-7 bg-gradient-to-l from-purple-50 to-blue-50">
           POST
         </p>
-        <p className="my-4 border-l-4 border-white pl-7">ARCHIVE</p>
-        <p className="my-4 border-l-4 border-white pl-7">CATEGORY</p>
+        <p className="my-2 py-4 border-l-4 border-white pl-7 transition-colors delay-100 hover:border-blue-300">
+          ARCHIVE
+        </p>
+        <p className="my-2 py-4 border-l-4 border-white pl-7 transition-colors delay-100 hover:border-blue-300">
+          CATEGORY
+        </p>
       </div>
       <div className="absolute text-2xl bottom-0 right-0 w-full">
         <Pagination
