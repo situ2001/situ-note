e4ad33ed7d211d1d72a693fb88df028eb86bc258
situ2001 <yongcong2001@outlook.com>
2021-09-13 23:23:28 +0800

    feat(index): personal info card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react';
-import Layout from '../components/Layout';
-import Container from '../components/Container';
+import { StaticImage } from 'gatsby-plugin-image';
+import { FiPenTool, FiGithub, FiInfo } from 'react-icons/fi';
+import { Link } from 'gatsby';
 
-type Props = {
-  location: Location;
-};
-
-const Index = (props: Props) => {
-  const { location } = props;
-
+const Index = () => {
+  // TODO: Personal Card
   return (
-    <Layout location={location}>
-      <Container>
-        <p>Welcome to my site!</p>
-        <p>Still under intense development.</p>
-      </Container>
-    </Layout>
+    <div className="container flex min-h-screen p-8 justify-center md:items-center">
+      <div className="w-full h-full md:shadow-lg md:rounded-2xl md:max-w-screen-sm">
+        {/* Card starts */}
+        <div className="max-w-md mx-auto w-full flex flex-col justify-center md:p-8">
+          {/* avatar & nickname */}
+          <div className="mx-auto my-4 self-center">
+            <div className="w-24 h-auto">
+              <StaticImage
+                src="../images/28241963.jpg"
+                alt="test"
+                className="rounded-full"
+              />
+            </div>
+            <p className="text-center mt-2">situ2001</p>
+          </div>
+          {/* intro */}
+          <p className="text-center">Welcome to my site</p>
+          {/* Links */}
+          <div className="flex justify-center m-4">
+            <Link to="/blog" className="mx-2">
+              <FiPenTool />
+            </Link>
+            <Link to="https://github.com/situ2001" className="mx-2">
+              <FiGithub />
+            </Link>
+            <Link to="/about" className="mx-2">
+              <FiInfo />
+            </Link>
+          </div>
+        </div>
+        {/* Card ends */}
+      </div>
+    </div>
   );
 };
 
