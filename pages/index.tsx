f53f9201d4c2fe664cab55afb6e430c0fd784ef0
situ2001 <yongcong2001@outlook.com>
2022-02-05 19:15:32 +0800

    feat: add announcement to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import useDocumentTitle from "../hooks/useDocumentTitle";
+import EngineeringIcon from "@mui/icons-material/Engineering";
+import { Box, Container, Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   useDocumentTitle("Home");
 
   return (
     <Layout>
-      <></>
+      <Container>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <EngineeringIcon sx={{ height: "50%", width: "50%" }} color="disabled" />
+          <Typography component="div" variant="h3">仍在开发中</Typography>
+          <Typography component="div" variant="h5">阅读博客请点击右上角BLOG</Typography>
+        </Box>
+      </Container>
     </Layout>
   );
 };
