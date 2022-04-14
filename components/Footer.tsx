f18e9ace26c3491d8604e60ff02d869bc8e473a3
situ2001 <yongcong2001@outlook.com>
2022-04-14 21:08:22 +0800

    change footer opacity

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className="flex justify-center my-4">
+      <div className="flex justify-center my-4 opacity-50">
         Made by&nbsp;
         <a href="https://github.com/situ2001">situ2001</a>
       </div>
