1a53bc3586d042e72dfd0a643d30db2ea5eaf2c5
situ2001 <yongcong2001@outlook.com>
2022-04-15 00:30:49 +0800

    feat(Layout): options flex & center

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,27 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
-// TODO 添加 Footer
-const Layout = (props: { children: React.ReactNode }) => {
-  const { children } = props;
+const Layout = ({
+  children,
+  flex = false,
+  center = false,
+  hideHeader = false,
+}: {
+  children: React.ReactNode;
+  flex?: boolean;
+  center?: boolean;
+  hideHeader?: boolean;
+}) => {
   return (
     <div className="px-4 md:px-16 flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1">{children}</main>
+      {!hideHeader && <Header />}
+      <main
+        className={`flex-1 ${
+          flex && `flex flex-col ${center && `items-center`}`
+        }`}
+      >
+        {children}
+      </main>
       <Footer />
     </div>
   );
