07ba0f240fd92e982c35c0c89b0a5600a082189e
situ2001 <yongcong2001@outlook.com>
2022-04-17 20:04:37 +0800

    feat: update `next` to 12.1.5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import "@fontsource/noto-sans-mono";
 import "@fontsource/noto-sans-sc";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { pathname } = req.nextUrl;
   if (pathname === "/page") {
-    return NextResponse.redirect("/page/1");
+    return NextResponse.redirect(new URL("/page/1", req.url));
   }
   return NextResponse.next();
 }
