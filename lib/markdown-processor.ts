e1a5e7d92f33b5dd0f48e122fda8f149f5ad5752
situ2001 <yongcong2001@outlook.com>
2022-04-17 18:00:09 +0800

    delete: remove `rehype-document`

--- lib/markdown-processor.ts
+++ lib/markdown-processor.ts
@@ -3,7 +3,6 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypeDocument from "rehype-document";
 import rehypeHighlight from "rehype-highlight";
 import groovy from "highlight.js/lib/languages/groovy";
 import powershell from "highlight.js/lib/languages/powershell";
@@ -16,6 +15,4 @@ export const markdownProcessor = unified()
   .use(rehypeHighlight, { languages: { groovy, powershell } })
   .use(remarkMath)
   .use(rehypeKatex)
-  .use(rehypeDocument, {
-    css: [],
-  });
+  
