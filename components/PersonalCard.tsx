d53c8cc8f07151493fcbf0f5c798a8ed6a53dbf0
situ2001 <yongcong2001@outlook.com>
2022-07-02 15:11:54 +0800

    refactor: merge component

--- components/PersonalCard.tsx
+++ components/PersonalCard.tsx
@@ -1,4 +1,4 @@
-import Greeting from "./Greeting";
+/* eslint-disable @next/next/no-img-element */
 import {
   FiPenTool,
   FiGithub,
@@ -8,6 +8,7 @@ import {
   FiHelpCircle,
 } from "react-icons/fi";
 import { IconList } from "../types/PersonalCard";
+import Link from "next/link";
 
 const iconList: IconList = [
   {
@@ -37,16 +38,40 @@ const column = {
   },
 };
 
+const name = "situ2001";
+const avatarLink = "https://avatars.githubusercontent.com/u/28241963";
+
 const PersonalCard = ({ githubStat }: any) => {
   const { stars, prs, issues, contributeTo } = githubStat;
 
   return (
     <div className="flex flex-col w-full max-w-md my-8">
-      <Greeting
-        name="situ2001"
-        avatarLink="https://avatars.githubusercontent.com/u/28241963"
-        iconList={iconList}
-      />
+      <div className="flex max-h-36 justify-between mb-8">
+        <div className="flex flex-col justify-end flex-1">
+          <p className="text-4xl font-extrabold w-full">Hi</p>
+          <p className="text-2xl w-full">I am {name}</p>
+          <p className="mt-2 opacity-50 text-xs">
+            You may be interested in my...
+          </p>
+          <div className="mt-2 flex">
+            {iconList.map((obj, i) => (
+              <>
+                <Link key={i} href={obj.href}>
+                  {obj.element}
+                </Link>
+                <div className="mx-1"></div>
+              </>
+            ))}
+          </div>
+        </div>
+        <div className="ml-4 flex-grow-0 max-w-full h-36">
+          <img
+            className="rounded-full object-cover h-full max-w-full"
+            src={avatarLink}
+            alt="avatar"
+          />
+        </div>
+      </div>
       <div>
         <h1 className="font-bold text-xl mb-2">Intro</h1>
         {column.introduction.text.map((text) => (
