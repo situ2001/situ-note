1e03b408c3211ef3d5f82d7598bb84d3260d0a60
situ2001 <yongcong2001@outlook.com>
2022-08-22 14:05:35 +0800

    add running time to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import moment from "moment";
+import React, { useEffect, useMemo } from "react";
 
 const Footer = () => {
+  const duration = useMemo(
+    () =>
+      moment
+        .duration(moment(Date.now()).diff(moment("2020-09-12")))
+        .asDays()
+        .toFixed(),
+    []
+  );
+
   return (
     <footer>
-      <div className="flex justify-center my-4 opacity-50">
-        Made by&nbsp;
-        <a href="https://github.com/situ2001">situ2001</a>
+      <div className="flex flex-col justify-center items-center my-4 opacity-50">
+        <div>
+          Made by&nbsp;<a href="https://github.com/situ2001">situ2001</a>
+        </div>
+        <div>Running for {duration} days</div>
       </div>
     </footer>
   );
