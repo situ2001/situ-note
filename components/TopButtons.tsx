3e41c7fb209a8f15a15ed4899fdbfdcdf2b5f454
situ2001 <yongcong2001@outlook.com>
2022-08-22 14:28:49 +0800

    completed header buttons

--- components/TopButtons.tsx
+++ components/TopButtons.tsx
@@ -1,24 +1,88 @@
 import Link from "next/link";
 import React from "react";
-import { FiGithub, FiPenTool } from "react-icons/fi";
+import { FiInfo, FiMenu, FiPenTool } from "react-icons/fi";
 import DarkMode from "./DarkMode";
 
+interface ILinkedButton {
+  href: string;
+  icon: React.ReactElement;
+  text: string;
+  primary: boolean;
+  showTextOnMobile?: boolean;
+}
+
+const LinkedButton = ({
+  href,
+  icon,
+  text,
+  showTextOnMobile = false,
+}: ILinkedButton) => (
+  <React.Fragment>
+    <Link href={href}>
+      <div className="btn btn-ghost">
+        {icon}
+        <div
+          className={`ml-2 ${!showTextOnMobile ? "hidden md:block" : "block"}`}
+        >
+          {text}
+        </div>
+      </div>
+    </Link>
+  </React.Fragment>
+);
+
 const TopButtons = () => {
+  const buttons: ILinkedButton[] = [
+    {
+      href: "/page/",
+      icon: <FiPenTool size={20} />,
+      text: "Blog",
+      primary: true,
+    },
+    {
+      href: "/about/",
+      icon: <FiInfo size={20} />,
+      text: "About",
+      primary: false,
+    },
+  ];
+
   return (
     <React.Fragment>
-      <Link href="https://github.com/situ2001">
-        <div className="btn btn-ghost">
-          <FiGithub size={20} />
-          <div className="ml-2 hidden md:block">Github</div>
-        </div>
-      </Link>
-      <Link href="/page/">
-        <div className="btn btn-ghost">
-          <FiPenTool size={20} />
-          <div className="ml-2 hidden md:block">Blog</div>
-        </div>
-      </Link>
+      {buttons
+        .filter((v) => v.primary)
+        .map((button, i) => (
+          <LinkedButton {...button} key={i} />
+        ))}
+
+      <div className="hidden md:block">
+        {buttons
+          .filter((v) => !v.primary)
+          .map((button, i) => (
+            <LinkedButton {...button} key={i} />
+          ))}
+      </div>
+
       <DarkMode />
+
+      <div className="dropdown dropdown-end block md:hidden">
+        <div tabIndex={0} className="btn btn-ghost ">
+          <FiMenu size={20} />
+          <div className="ml-2 hidden md:block">Menu</div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu shadow bg-base-100 rounded-box p-4 w-52"
+        >
+          {buttons.map((button, i) =>
+            button.primary ? null : (
+              <li key={i}>
+                <LinkedButton {...button} showTextOnMobile />
+              </li>
+            )
+          )}
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
