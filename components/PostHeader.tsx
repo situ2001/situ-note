37d894a0265e884fb4f82d25c6cce6fb1c8522aa
situ2001 <yongcong2001@outlook.com>
2022-09-13 16:44:56 +0800

    feat: add basic view count

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -1,7 +1,9 @@
 import { Post } from "@prisma/client";
 import moment from "moment";
-import React from "react";
-import type { FrontMatter } from "../types/BlogPost";
+import React, { useEffect, useState } from "react";
+import useSWR, { mutate } from "swr";
+import { getFetcher, postFetcher } from "../lib/fecther";
+import { ViewResponse } from "../pages/api/views/[slug]";
 
 type Props = {
   frontMatter: Post;
@@ -15,12 +17,25 @@ const PostHeader = ({ frontMatter }: Props) => {
   const realTimeUTC = dateObject.getTime();
   const currentMoment = moment(realTimeUTC);
 
+  const { data, mutate } = useSWR<ViewResponse>(
+    `/api/views/${frontMatter.filename}`,
+    getFetcher
+  );
+
+  // increase view
+  useEffect(() => {
+    fetch(`/api/views/${frontMatter.filename}`, { method: "POST" }).then(
+      // () => mutate() // TODO mutate or not
+    );
+  }, [frontMatter.filename, mutate]);
+
   return (
     <React.Fragment>
       <h1 className="text-4xl font-extrabold my-2 text-center">
         {frontMatter.title}
       </h1>
       <p className="text-center">{currentMoment.format("ll dddd")}</p>
+      <p className="text-center">{data ? `${data.count} views` : 'Loading views'} </p>
     </React.Fragment>
   );
 };
---
+++ pages/api/views/[slug].ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getCount, increaseCounter } from "../../../lib/views";
+import prisma from "../../../lib/prisma";
+
+export interface ViewResponse {
+  message: string;
+  count?: number;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ViewResponse>
+) {
+  const slug = req.query.slug?.toString();
+  if (!slug) {
+    res.status(400).json({ message: "Invalid slug" });
+    return;
+  }
+
+  // check slug is in local db
+  const hasSlug = await prisma.post.findFirst({ where: { filename: slug } });
+  if (!hasSlug) {
+    res.status(400).json({ message: "Such slug is not found" });
+    return;
+  }
+
+  console.log(slug);
+  if (req.method === "POST") {
+    // increase
+    await increaseCounter(slug!);
+  }
+  // get counter
+  const count = await getCount(slug!);
+  if (count === undefined) {
+    res.status(500).json({ message: "Internal server error" });
+  } else {
+    res.status(200).json({ message: "OK", count });
+  }
+}
