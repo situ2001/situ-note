4206b746787b4dedc02d626e886472752dda3074
situ2001 <yongcong2001@outlook.com>
2023-10-23 15:06:46 +0800

    feat: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://situ2001.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   image: {
     service: {
-      entrypoint: "src/api/image.ts",
-    },
-  },
+      entrypoint: "src/api/image.ts"
+    }
+  }
 });
