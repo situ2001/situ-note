156c05ac3aae16e16870614977c5a3af69481a9c
situ2001 <yongcong2001@outlook.com>
2023-10-23 22:17:39 +0800

    feat: brand new friend card

---
+++ src/pages/friends/ config.ts
@@ -0,0 +1,18 @@
+export const friends = [
+  {
+    name: "Airing",
+    link: "https://ursb.me",
+  },
+  {
+    name: "EMimTY",
+    link: "https://www.eaimty.com",
+  },
+  {
+    name: "Yuzi",
+    link: "https://blog.yuzi.dev",
+  },
+  {
+    name: "RH-Xie",
+    link: "https://rhxie.top",
+  },
+];
