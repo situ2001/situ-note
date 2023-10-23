86f4df2e9fc90d6e15c51f1166de826d0e367f64
situ2001 <yongcong2001@outlook.com>
2023-10-23 12:23:26 +0800

    feat: init astro

--- pages/about/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from "../../components/Layout";
-
-export default function About() {
-  return <Layout>This is about page</Layout>;
-}
--- scripts/constant.mjs
+++
@@ -1,3 +0,0 @@
-import path from "path";
-
-export const cloneTargetDir = path.join(process.cwd(), "posts-clone");
