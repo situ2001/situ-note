026641fc00323907c1e9f8295c621c4ff3c7585d
situ2001 <yongcong2001@outlook.com>
2023-10-23 12:34:45 +0800

    chore: modify indent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://situ2001.com',
+  site: "https://situ2001.com",
   integrations: [mdx(), sitemap()],
 });
