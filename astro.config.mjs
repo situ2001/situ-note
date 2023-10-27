77c03eba017f3a3e702c8955af316576a67726e0
situ2001 <yongcong2001@outlook.com>
2023-10-27 21:00:56 +0800

    feat: use solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
-
 import prefectch from "@astrojs/prefetch";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://situ2001.com",
-  integrations: [mdx(), sitemap(), tailwind(), prefectch()],
+  integrations: [mdx(), sitemap(), tailwind(), prefectch(), solidJs()],
   image: {
     service: {
-      entrypoint: "src/api/image.ts",
-    },
-  },
+      entrypoint: "src/api/image.ts"
+    }
+  }
 });
