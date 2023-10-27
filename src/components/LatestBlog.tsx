fa3b3d1f7f429b2b65363ca4c1e2beae8c628237
situ2001 <yongcong2001@outlook.com>
2023-10-27 21:04:21 +0800

    fix(latest-blog): use pointer event

--- src/components/LatestBlog.tsx
+++ src/components/LatestBlog.tsx
@@ -9,14 +9,14 @@ type Post = (typeof posts)[0];
 
 export const LatestBlog = () => {
   const [hoveredElement, setHoveredElement] = createSignal<Post | null>(null);
-  const [isMouseOverBlogList, setIsMouseOver] = createSignal(false);
+  const [isPointerOverBlogList, setIsPointerOverBlogList] = createSignal(false);
 
   return (
     <div class="h-full flex flex-col">
       <header
         class="text-xl font-bold transition-all duration-150"
         style={{
-          opacity: !isMouseOverBlogList() ? 1 : 0.3,
+          opacity: !isPointerOverBlogList() ? 1 : 0.3,
         }}
       >
         Latest Blog Posts
@@ -24,18 +24,18 @@ export const LatestBlog = () => {
       <hr class="my-1" />
       {/* <section class="overflow-scroll flex-1"> */}
       <ul
-        onMouseEnter={() => setIsMouseOver(true)}
-        onMouseLeave={() => setIsMouseOver(false)}
+        onPointerEnter={() => setIsPointerOverBlogList(true)}
+        onPointerLeave={() => setIsPointerOverBlogList(false)}
         class="flex flex-col justify-between flex-1"
       >
         {posts.map((post) => (
           <div
-            onMouseEnter={() => setHoveredElement(post)}
-            onMouseLeave={() => setHoveredElement(null)}
+            onPointerEnter={() => setHoveredElement(post)}
+            onPointerLeave={() => setHoveredElement(null)}
             class={`my-1 transition-all duration-150 hover:scale-105 origin-left`}
             style={{
               opacity:
-                !isMouseOverBlogList() || hoveredElement() === post ? 1 : 0.3,
+                !isPointerOverBlogList() || hoveredElement() === post ? 1 : 0.3,
             }}
           >
             <a href={`/blog/${post.slug}`}>
