03082162a0975b78f8d31f81d17a2901e69f5b9d
situ2001 <yongcong2001@outlook.com>
2023-10-29 19:39:54 +0800

    fix(img): apply style only on blog post content

--- src/api/image.ts
+++ src/api/image.ts
@@ -10,12 +10,7 @@ const service: LocalImageService = {
     const ret = baseService.getHTMLAttributes?.(options, imageConfig);
 
     // hook, add inline style
-    const inlineStyle = `
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      object-position: center;
-    `;
+    const inlineStyle = ``; // tmp remove
 
     return { ...ret, style: inlineStyle } ?? {};
   },
