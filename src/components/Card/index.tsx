ca9cd8b0bf789ba96b67c5b42a4b714f3f2f4d88
situ2001 <yongcong2001@outlook.com>
2023-10-29 19:27:19 +0800

    refactor(component): remove unused code

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,20 +9,6 @@ import {
 import styles from "./style.module.css";
 
 const Card = (props: any) => {
-  const [rotate, setRotate] = createSignal(217);
-
-  const linearGradientStyle = createMemo(
-    () => `
-  background: linear-gradient(${rotate()}deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
-  linear-gradient(${
-    rotate() - 90
-  }deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
-  linear-gradient(${
-    rotate() + 120
-  }deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);
-  `
-  );
-
   return (
     <div
       class={
@@ -30,8 +16,6 @@ const Card = (props: any) => {
         " " +
         "bg-white rounded-xl shadow-lg p-4 transition-all mb-4"
       }
-      // class="bg-white rounded-xl shadow-lg p-4 w-64 h-64 transition-all duration-200 ease-in-out hover:scale-105"
-      // style={linearGradientStyle()}
     >
       {props.children}
     </div>
