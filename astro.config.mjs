4bec18d55c66ea15ec414733211273d6e9fdff16
situ2001 <yongcong2001@outlook.com>
2023-11-11 10:30:32 +0800

    feat: add styles for code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,24 @@ export default defineConfig({
       entrypoint: "src/api/image.ts",
     },
   },
+  markdown: {
+    // TODO just copy from official doc, using blog-post.css now
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "github-light",
+      // Alternatively, provide multiple themes
+      // https://github.com/antfu/shikiji#lightdark-dual-themes
+      experimentalThemes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: false,
+    },
+  },
 });
