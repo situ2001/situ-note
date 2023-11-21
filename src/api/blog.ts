d757b6148d1ec037a0bb0a1059c0777aed0786ca
situ2001 <yongcong2001@outlook.com>
2023-11-21 09:53:15 +0800

    feat: init category

--- src/api/blog.ts
+++ src/api/blog.ts
@@ -1,4 +1,5 @@
 import { getCollection, type CollectionEntry } from "astro:content";
+import groupBy from "lodash/groupBy";
 
 // init once, because it's a static collection
 const posts = await getCollection("blog");
@@ -37,4 +38,30 @@ export const getStaticPaths = () => {
 
 export const getPostsForRSS = () => {
   return getPostSortedByDate().map(convertToUTC8);
+};
+
+/**
+ * Group by categories, which is a string with comma separated
+ * Notice that the category is flattened, so each post may appear multiple times
+ * @returns
+ */
+export const getPostsGroupByCategory = () => {
+  const flattenCategoryPosts = getPostSortedByDate().flatMap((post) => {
+    return post.data.categories
+      .split(",")
+      .map((s) => s.trim())
+      .map((category) => {
+        return { ...post, data: { ...post.data, categories: category } };
+      });
+  });
+  const c = groupBy(flattenCategoryPosts, (post) => post.data.categories);
+  return c;
+};
+
+export const getCategoryStaticPaths = () => {
+  const categories = getPostsGroupByCategory();
+  return Object.keys(categories).map((category) => ({
+    params: { category },
+    props: { posts: categories[category].map(convertToUTC8), category },
+  }));
 };
---
+++ src/components/Categories.tsx
@@ -0,0 +1,29 @@
+import { getPostsGroupByCategory } from "../api/blog";
+
+const posts = getPostsGroupByCategory();
+
+export const Category = () => {
+  return (
+    <div class="h-full flex flex-col">
+      {/* TODO not now but future */}
+      {/* <a href="/blog/category"> */}
+      <header class="text-xl font-bold">Category</header>
+      {/* </a> */}
+      <hr class="my-1" />
+      <div class="my-1">
+        {Object.keys(posts).map((category) => {
+          return (
+            <span class="mr-4 inline-block relative">
+              <a
+                class="hover:underline underline-offset-4"
+                href={`/blog/category/${category}`}
+              >
+                {category}
+              </a>
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
