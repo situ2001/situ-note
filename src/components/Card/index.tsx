4d41b6c43de9a11932bc97094d9f1082cb7f8653
situ2001 <yongcong2001@outlook.com>
2024-01-17 11:03:03 +0800

    feat(home): add animation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,11 +8,41 @@ import {
 
 import styles from "./style.module.css";
 
-const Card = (props: any) => {
+export enum FadeFromDirection {
+  Top,
+  Right,
+  Bottom,
+  Left,
+}
+
+interface CardProps {
+  fadeFromDirection?: FadeFromDirection;
+  fullHeight?: boolean;
+}
+
+const Card = (props: CardProps & { children?: any }) => {
+  const directionClassName = createMemo(() => {
+    switch (props.fadeFromDirection) {
+      case FadeFromDirection.Top:
+        return styles.fadeFromTop;
+      case FadeFromDirection.Right:
+        return styles.fadeFromRight;
+      case FadeFromDirection.Bottom:
+        return styles.fadeFromBottom;
+      case FadeFromDirection.Left:
+        return styles.fadeFromLeft;
+      default:
+        return styles.fadeFromBottom;
+    }
+  });
+
   return (
     <div
+      style={{
+        height: props.fullHeight ? "100%" : "auto",
+      }}
       class={
-        styles.root +
+        directionClassName() +
         " " +
         "bg-white rounded-xl shadow-lg p-4 transition-all mb-4"
       }
