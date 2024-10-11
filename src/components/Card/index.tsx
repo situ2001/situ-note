4ed3501e02f7c346eb8ece94a3923bd039f14b8c
situ2001 <yongcong2001@outlook.com>
2024-10-11 22:00:43 +0800

    chore: remove animation from home page

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,51 +1,14 @@
-import {
-  createComputed,
-  createEffect,
-  createMemo,
-  createRenderEffect,
-  createSignal,
-} from "solid-js";
-
-import styles from "./style.module.css";
-
-export enum FadeFromDirection {
-  Top,
-  Right,
-  Bottom,
-  Left,
-}
-
 interface CardProps {
-  fadeFromDirection?: FadeFromDirection;
   fullHeight?: boolean;
 }
 
 const Card = (props: CardProps & { children?: any }) => {
-  const directionClassName = createMemo(() => {
-    switch (props.fadeFromDirection) {
-      case FadeFromDirection.Top:
-        return styles.fadeFromTop;
-      case FadeFromDirection.Right:
-        return styles.fadeFromRight;
-      case FadeFromDirection.Bottom:
-        return styles.fadeFromBottom;
-      case FadeFromDirection.Left:
-        return styles.fadeFromLeft;
-      default:
-        return styles.fadeFromBottom;
-    }
-  });
-
   return (
     <div
       style={{
         height: props.fullHeight ? "100%" : "auto",
       }}
-      class={
-        directionClassName() +
-        " " +
-        "bg-white dark:bg-gray-700 rounded-xl shadow-lg p-4 transition-transform mb-4"
-      }
+      class="bg-white dark:bg-gray-700 rounded-xl shadow-lg p-4 transition-transform mb-4"
     >
       {props.children}
     </div>
