bf7d729aaed9b97f7e70ec01d5ebed12297cf654
situ2001 <yongcong2001@outlook.com>
2024-10-11 21:37:26 +0800

    refactor: extract logics

---
+++ src/components/ReadingProgress.tsx
@@ -0,0 +1,39 @@
+import { createEffect, createSignal } from "solid-js";
+
+export interface ReadingProgressProps {
+  querySelector: string;
+}
+
+export default function ReadingProgress(props: ReadingProgressProps) {
+  let { querySelector } = props;
+
+  const [readingProgress, setReadingProgress] = createSignal(0);
+  const getReadingProgress = () => {
+    const contentRef = document.querySelector(querySelector);
+
+    if (!contentRef) return;
+
+    // compute reading progress
+    let contentHeight = contentRef.clientHeight;
+    let documentOffset = document.documentElement.scrollTop;
+    let contentHeightForComputation = contentHeight - window.outerHeight; // base line is the top of the viewport when scrolled to the bottom
+
+    let progress = Math.floor((documentOffset / contentHeightForComputation) * 100);
+
+    // restrict progress to 0-100
+    progress = Math.min(progress, 100);
+    progress = Math.max(progress, 0);
+
+    setReadingProgress(progress);
+  };
+
+  createEffect(() => {
+    getReadingProgress();
+    window.addEventListener("scroll", getReadingProgress);
+    return () => {
+      window.removeEventListener("scroll", getReadingProgress);
+    };
+  });
+
+  return <div>Read {readingProgress()}%</div>
+}
---
+++ src/utils/pluralize.ts
@@ -0,0 +1,6 @@
+import pluralize from 'pluralize';
+
+// add custom pluralization rules here
+// pluralize.addUncountableRule('Category');
+
+export default pluralize;
