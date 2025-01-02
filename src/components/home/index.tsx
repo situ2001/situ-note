99c207400535867fb43b6c24afbaf07712f05554
situ2001 <yongcong2001@outlook.com>
2025-01-02 13:16:35 +0800

    refactor: remove Contact component and update imports accordingly

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,6 @@
 // Main components for portfolio
 export { default as AboutMe } from './AboutMe';
 export { default as Roles } from './Roles';
-export { default as Contact } from './Contact';
 
 // Blog-related components
 export { default as Blog } from './Blog';
