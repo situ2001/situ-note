fec371c385c93fadd5655d117c35cb7a94109be7
situ2001 <yongcong2001@outlook.com>
2025-05-11 17:32:24 +0800

    fix: import

--- src/modules/home/index.ts
+++ src/modules/home/index.ts
@@ -2,9 +2,6 @@
 export { default as HeroSection } from './HeroSection';
 export { default as Roles } from './Roles';
 
-// Projects-related components
-export { default as Projects } from './Projects.astro';
-
 // Blog-related components
 export { default as Blog } from './Blog';
 export { default as Categories } from './Categories';
