4de03aa3c64c564c954890ad4894130a9d478b67
situ2001 <yongcong2001@outlook.com>
2025-05-31 17:32:16 +0800

    fix: typo

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 const Card = ({ children, className }: CardProps) => {
   return (
     <div className={clsx(
-      "p-2  rounded-xl",
+      "p-2 rounded-xl",
       "bg-zinc-100 dark:bg-zinc-800",
       "border-2 border-transparent hover:border-zinc-200 hover:dark:border-zinc-700",
       "transition-all duration-200 ease-in-out",
