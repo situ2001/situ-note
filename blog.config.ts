c37b72e114c9319097c0d85d3e9e9a9dd4520339
situ2001 <yongcong2001@outlook.com>
2026-06-20 22:23:34 +0800

    add friend

--- blog.config.ts
+++ blog.config.ts
@@ -1,8 +1,28 @@
-import type { BlogConfig } from './types';
+import type { BlogConfig } from "./types";
 
-import { BsGithub, BsTwitterX, BsInstagram, BsEnvelopeAt, BsTelegram, BsLinkedin, BsThreads } from 'react-icons/bs';
+import {
+  BsGithub,
+  BsTwitterX,
+  BsInstagram,
+  BsEnvelopeAt,
+  BsTelegram,
+  BsLinkedin,
+  BsThreads,
+} from "react-icons/bs";
 import { BsRss } from "react-icons/bs";
-import { Friendship, Keyboard, GroupObjects, Migrate, Query, CharacterFraction, PromptTemplate, Automatic, DirectionBearRight_02, Calendar, Stamp } from '@carbon/icons-react';
+import {
+  Friendship,
+  Keyboard,
+  GroupObjects,
+  Migrate,
+  Query,
+  CharacterFraction,
+  PromptTemplate,
+  Automatic,
+  DirectionBearRight_02,
+  Calendar,
+  Stamp,
+} from "@carbon/icons-react";
 
 const config: BlogConfig = {
   title: "situ2001",
@@ -18,76 +38,94 @@ const config: BlogConfig = {
     ],
     contacts: [
       { name: "GitHub", link: "https://github.com/situ2001", icon: BsGithub },
-      { name: "Twitter/X", link: "https://twitter.com/situ2oo1", icon: BsTwitterX },
+      {
+        name: "Twitter/X",
+        link: "https://twitter.com/situ2oo1",
+        icon: BsTwitterX,
+      },
       { name: "Telegram", link: "https://t.me/situ2001", icon: BsTelegram },
       // { name: "Instagram", link: "https://www.threads.com/@situ2oo1", icon: BsThreads },
-      { name: "LinkedIn", link: "https://www.linkedin.com/in/situ2001/", icon: BsLinkedin },
-    ]
+      {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/situ2001/",
+        icon: BsLinkedin,
+      },
+    ],
   },
 
   nav: {
     items: [
-      { name: "Blog", link: "/blog", },
+      { name: "Blog", link: "/blog" },
       // { name: "Insights", link: "/insight", },
       { name: "Projects", link: "/projects" },
-      { name: "Friends", link: "/friends", icon: Friendship, autoMinimal: true },
+      {
+        name: "Friends",
+        link: "/friends",
+        icon: Friendship,
+        autoMinimal: true,
+      },
       { name: "RSS", link: "/rss.xml", icon: BsRss, forceMinimal: true },
-    ]
+    ],
   },
 
   projects: [
     {
       title: "KeyPhantom",
-      description: "Send keyboard events silently to background macOS applications.",
+      description:
+        "Send keyboard events silently to background macOS applications.",
       link: "https://github.com/situ2001/KeyPhantom",
       icon: Keyboard,
-      featured: true
+      featured: true,
     },
     {
       title: "Unplugin MCP",
-      description: "Let AI know more about your code and build process from JS bundlers.",
+      description:
+        "Let AI know more about your code and build process from JS bundlers.",
       link: "https://github.com/situ2001/unplugin-mcp",
       icon: PromptTemplate,
-      featured: true
+      featured: true,
     },
     {
       title: "Obsidian Tab Group Arrangement",
       description: "Arrange your tabs in Obsidian like vscode.",
       link: "https://github.com/situ2001/obsidian-tab-group-arrangement",
       icon: GroupObjects,
-      featured: true
+      featured: true,
     },
     {
       title: "gitea-bulk-migration",
       description: "CLI for bulk migration of repositories to Gitea.",
       link: "https://github.com/situ2001/gitea-bulk-migration",
       icon: Migrate,
-      featured: true
+      featured: true,
     },
 
     {
       title: "which-npm",
-      description: "CLI for locating the npm package of a given filename or path.",
+      description:
+        "CLI for locating the npm package of a given filename or path.",
       link: "https://github.com/situ2001/which-npm",
-      icon: Query
+      icon: Query,
     },
     {
       title: "oh-pluralize",
       description: "Pluralize library for OpenHarmony.",
       link: "https://github.com/situ2001/oh-pluralize",
-      icon: CharacterFraction
+      icon: CharacterFraction,
     },
     {
       title: "auto-adb",
-      description: "Automatically execute commands when Android device is adb-connected.",
+      description:
+        "Automatically execute commands when Android device is adb-connected.",
       link: "https://github.com/situ2001/auto-adb",
-      icon: Automatic
+      icon: Automatic,
     },
     {
       title: "scoped-rem",
-      description: "Webpack loader that transforms rem units to be relative to a custom root font size.",
+      description:
+        "Webpack loader that transforms rem units to be relative to a custom root font size.",
       link: "https://github.com/situ2001/scoped-rem",
-      icon: DirectionBearRight_02
+      icon: DirectionBearRight_02,
     },
     {
       title: "nth-week",
@@ -97,10 +135,11 @@ const config: BlogConfig = {
     },
     {
       title: "picmark",
-      description: "Add watermarks to your images in just a few lines of command or code.",
+      description:
+        "Add watermarks to your images in just a few lines of command or code.",
       link: "https://github.com/situ2001/picmark",
       icon: Stamp,
-    }
+    },
   ],
 
   friends: [
@@ -113,15 +152,16 @@ const config: BlogConfig = {
     { name: "younggglcy", link: "https://www.younggglcy.com/" },
     { name: "Talaxy", link: "https://www.talaxy.site/" },
     { name: "Lakphy", link: "https://lakphy.me/" },
-    { name: 'cos', link: 'https://ysx.cosine.ren/' },
-    { name: 'Corey Chiu', link: 'https://coreychiu.com' },
-    { name: 'HuanXin', link: 'https://huanxin-chen.github.io/' },
-    { name: 'lz233', link: 'https://with.fish/' },
+    { name: "cos", link: "https://ysx.cosine.ren/" },
+    { name: "Corey Chiu", link: "https://coreychiu.com" },
+    { name: "HuanXin", link: "https://huanxin-chen.github.io/" },
+    { name: "lz233", link: "https://with.fish/" },
     { name: "liruifengv", link: "https://liruifengv.com" },
     { name: "Guoqi Sun", link: "https://blog.guoqi.dev" },
-    { name: 'Charles', link: 'https://www.charles-cheng.com/' },
-    { name: 'Zhongye', link: 'https://zhongye.github.io' }
-  ]
-}
+    { name: "Charles", link: "https://www.charles-cheng.com/" },
+    { name: "Zhongye", link: "https://zhongye.github.io" },
+    { name: "HoHouman", link: "https://leidun.pp.ua" },
+  ],
+};
 
 export default config;
